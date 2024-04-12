a14f96ee6474b906f5cc1d82b6a815c72bf75263
Sang Lee <tkdgus2233440@gmail.com>
2024-04-11 21:27:07 -0600

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
+import React from "react";
+import { Styles } from "./styles";
+import Header from "./components/header";
+
 export default function Page() {
   return (
     <main>
-      <h1>Shopping List</h1>
+      <Header />
+      <select>
+        <option value="all">All</option>
+        <option value="indian">Indian</option>
+        <option value="korean">Korean</option>
+        <option value="chinese">Chinese</option>
+        <option value="vietnamese">Vietnamese</option>
+        <option value="western">Western</option>
+        <option value="thai">Thai</option>
+        <option value="european">European</option>
+      </select>
     </main>
   );
 }
---
+++ app/styles.js
@@ -0,0 +1,15 @@
+export const Styles = {
+  h1: {
+    color: "red",
+  },
+  header: {
+    color: "#186F65",
+    fontSize: "24px",
+    fontFamily: "Libre Baskerville",
+  },
+  headerContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "25px",
+  },
+};
