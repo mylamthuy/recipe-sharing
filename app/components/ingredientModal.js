292627889ae901bcea23e41fe7f790589629c9da
thuymy.lam <thuymylam96@gmail.com>
2024-04-17 23:54:33 -0600

    update new-post-form

---
+++ app/components/ingredientModal.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+
+export default function IngredientModal ({closeModal, onSave, defaultValue}) {
+    const [editedIngredient, setEditedIngredient] = useState(defaultValue || "");
+    const [error, setError] = useState("");
+
+    const handleChange = (e) => {
+        setEditedIngredient(e.target.value);
+        setError("");
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (editedIngredient.trim() === "") {
+            setError("Please fill in the ingredient !!");
+        } else {
+            onSave(editedIngredient);
+            closeModal();
+        }
+    };
+
+    return (
+        <div 
+            className="absolute inset-0 h-content w-full bg-gray-300/50 flex justify-center items-center"
+            onClick={closeModal}
+        >
+            <div 
+                className="w-full max-w-md bg-white p-6 rounded-lg shadow-md flex flex-col items-center"
+                onClick={(e) => {
+                    e.stopPropagation(); // prevent click from bubbling up to parent div
+                  }}>
+                <p className="text-lg font-libre-baskerville font-bold text-center content-color">Update Ingredient</p>
+                    <input
+                        className="w-11/12 p-2 block mt-4 mb-2 content-color font-roboto text-base rounded-lg secondary-background-color border border-color focus:border-2 focus:outline-none"
+                        name="ingredient"
+                        value={editedIngredient}
+                        onChange={handleChange}
+                    >
+                    </input>
+                    {error && <p className="text-red-600 text-sm font-roboto mb-4">{error}</p>}
+                    <button 
+                        className="w-24 p-2 font-roboto text-white tracking-wider rounded-lg green-background-color hover:font-bold hover:shadow-md"
+                        onClick={handleSubmit}
+                    >
+                        Save
+                    </button>
+            </div>
+        </div>
+    )
+}
--- app/components/label.js
+++ app/components/label.js
@@ -1,7 +1,7 @@
 export default function Label({ text }) {
     return (
-        <label className="text-2xl content-color font-libre-baskerville font-bold">
+        <p className="text-2xl content-color font-libre-baskerville font-bold">
             {text}
-        </label>
+        </p>
     );
   }
