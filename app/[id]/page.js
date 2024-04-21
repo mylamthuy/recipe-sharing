c71a85abe958234e5b8a0a6c7534316f7d2a162a
thuymy.lam <thuymylam96@gmail.com>
2024-04-20 23:05:18 -0600

    Update [id]/page.js

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -2,13 +2,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import PostInfo from "../components/post-info";
-import dishData from "../dishes.json";
 import { getPost } from "../_services/recipe-service";
 
 export default function Page({ params }) {
   const [post, setPost] = useState({});
 
-  const recipe = dishData[0];
 
   const fetchPost = async () => {
     const post = await getPost(params.id);
@@ -19,7 +17,6 @@ export default function Page({ params }) {
     fetchPost();
   }, [params.id]);
 
-  console.log(recipe);
   if (!post) {
     return (
       <div className="flex justify-center items-center ml-8 mt-12">
